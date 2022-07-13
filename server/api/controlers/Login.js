eedd9940c2aaf398ceac76f464e7ed3165200067
nechamish <nechami77@gmail.com>
2022-07-13 12:31:59 +0300

    Removed test directory

--- server/api/controlers/Login.js
+++
@@ -1,9 +0,0 @@
-module.exports={
-    Logins:(req,res)=>{
-        res.status(200).json(
-            {
-                message:'Login a new Users'
-            }
-        )
-    },
-}
--- server/api/routes/Login.js
+++
@@ -1,6 +0,0 @@
-// const express = require("express");
-// const rout = express.Router();
-
-// const { Logins } = require("../controlers/Login");
-
-// rout.post("/", Logins);
