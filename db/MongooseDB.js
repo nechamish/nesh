16e40cc78f81f562da27eef1697f64ed9a39705a
shoshizada <s0556737338@gmail.comgit clone https://github.com/shoshizada/WeightProject.git>
2022-07-18 18:40:08 +0300

    mongodb

--- db/MongooseDB.js
+++ db/MongooseDB.js
@@ -1,17 +1,17 @@
-// const mongoose = require("mongoose");
-// require('dotenv').config();
-// const CONNECTION_STRING = "mongodb://localhost:27017";
+const mongoose = require("mongoose");
+require('dotenv').config();
+const CONNECTION_STRING = "mongodb://localhost:27017";
 
-// class MongooseDB {
-//   constructor() {
+class MongooseDB {
+  constructor() {
 
-//   }
+  }
 
-//   async connect() {
-//     const url = `mongodb://localhost:${process.env.HOST || 27017}/${process.env.DB || "User"}`;
-//     console.log(url);
-//     await mongoose.connect(url);
-//     console.log(`mongoose DB connected!`);
-//   }
-// }
-// module.exports = new MongooseDB();
+  async connect() {
+    const url = `mongodb://localhost:${process.env.HOST || 27017}/${process.env.DB || "User"}`;
+    console.log(url);
+    await mongoose.connect(url);
+    console.log(`mongoose DB connected!`);
+  }
+}
+module.exports = new MongooseDB();
--- db/database.js
+++ db/database.js
@@ -1,25 +1,25 @@
-// const { MongoClient } = require("mongodb");
-// require("dotenv").config();
-// const connectionString = "mongodb://localhost:27017/test";
-// class dataBase {
-//   constructor() {}
-//   async connect() {
-//     const client = new MongoClient(connectionString, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const connectionString = "mongodb://localhost:27017/test";
+class dataBase {
+  constructor() {}
+  async connect() {
+    const client = new MongoClient(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-//     let connected = await client.connect();
-//     this.db = connected.db();
+    let connected = await client.connect();
+    this.db = connected.db();
 
-//     console.log("DB Connected!");
-//   }
-//   getDB() {
-//     return this.db;
-//   }
-// }
+    console.log("DB Connected!");
+  }
+  getDB() {
+    return this.db;
+  }
+}
 
-// module.exports = new dataBase();
+module.exports = new dataBase();
 
 
 
