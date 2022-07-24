0fcff1c5a205f4496f939cbd7b2c2938311eb5cf
nechamish <nechami77@gmail.com>
2022-07-24 14:48:10 +0300

    login

--- server/api/middleware/middleware.js
+++ server/api/middleware/middleware.js
@@ -1,10 +1,10 @@
 // const jwt = require("jsonwebtoken");
-const express = require("express");
-const app = express();
-const { auth } = require("express-oauth2-jwt-bearer");
+// const express = require("express");
+// const app = express();
+// const { auth } = require("express-oauth2-jwt-bearer");
 
-const checkJwt = auth({
-  audience: "YOUR_API_IDENTIFIER",
-  issuerBaseURL: `https://localhost:3000`,
-});
-module.exports = checkJwt;
+// const checkJwt = auth({
+//   audience: "YOUR_API_IDENTIFIER",
+//   issuerBaseURL: `https://localhost:3000`,
+// });
+// module.exports = checkJwt;
--- server/app.js
+++ server/app.js
@@ -3,14 +3,32 @@ const app = express();
 const cors = require('cors');
 const swaggerUi = require("swagger-ui-express"),
 swaggerDocument = require("../server/swagger.json");
-const checkJwt = require("../server/api/middleware/middleware");
+// const checkJwt = require("../server/api/middleware/middleware");
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 
 
 const db = require("./db/MongooseDB.js");
 const dotenv = require("dotenv");
 dotenv.config();
 db.connect();
-
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: 'a long, randomly-generated string stored in env',
+    baseURL: 'http://localhost:3000',
+    clientID: '2KbxwjVnxxBhonqXPREsLsXBsCm3kQVe',
+    issuerBaseURL: 'https://dev-i6vqgy24.us.auth0.com'
+  };
+  
+  // auth router attaches /login, /logout, and /callback routes to the baseURL
+  app.use(auth(config));
+  
+  // req.isAuthenticated is provided from the auth router
+  app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
+  
 
 // const dbc = require("./db/database.js");
 // dbc.connect();
@@ -30,8 +48,15 @@ app.use("/meeting/:id", MeetingRouter);
 app.use("/dairies/:id", DailyRouter);
 app.use("/dairies", DailyRouter);
 
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+  });
 app.listen(process.env.PORT || 3000, function () {
     console.log("SERVER STARTED PORT: 3000");})
+
+
+
+
 
 
 
